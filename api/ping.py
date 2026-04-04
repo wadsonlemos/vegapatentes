451f98b8739efd18ee75b328f07667fe67558877
Wadson <ulisses.lopes.de.oliveira.da.silva@kyndryl.com>
2026-04-04 18:25:57 -0300

    test: ping vercel endpoint

---
+++ api/ping.py
@@ -0,0 +1,9 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps({"ping": "pong"}).encode("utf-8"))
